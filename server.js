84950ddf16697ff66c04411f4693f4cd8cdc81e6
aseller39 <aseller39.studio@gmail.com>
2026-08-15 17:48:59 -0300

    adiciona webhook do WhatsApp

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  res.sendStatus(403);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
