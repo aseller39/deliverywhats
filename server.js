46689b16c57c59b4f8f99ffc3335d2e12a2f6403
aseller39 <aseller39.studio@gmail.com>
2026-08-31 07:48:52 -0300

    corrigindo cardapio da semana

--- server.js
+++ server.js
@@ -1820,7 +1820,7 @@ app.get("/api/cardapio", async (req, res) => {
       })
     );
 
-    const diaSemana = 2;
+    const diaSemana = hoje.getDay();
 
     const diaNaoTrabalhamos =
       diaSemana === 0 || diaSemana === 6;
