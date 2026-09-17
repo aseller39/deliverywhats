50d26aea92956d5bb347869e3813f5312de33de2
aseller39 <aseller39.studio@gmail.com>
2026-09-17 13:26:15 -0300

    corrigindo estoque de carnes

--- server.js
+++ server.js
@@ -2387,7 +2387,7 @@ app.get("/api/cardapio", async (req, res) => {
     );
 
     //const diaSemana = hoje.getDay();
-    const diaSemana = 2;
+    const diaSemana = hoje.getDay();
 
     const diaNaoTrabalhamos =
       diaSemana === 0 || diaSemana === 6;
